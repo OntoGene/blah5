f914613d689693a9477e5e9b465161bf9f83e144
Count Saint-Germain <sazerac@nikkou.local>
2019-02-14 18:20:53 +0900

    PubAnno can send requests

--- app.py
+++ app.py
@@ -40,21 +40,45 @@ def error_log(error,error_file=ERROR_FILE):
 # SERVER STUFF
 ##############
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
-	if request.method == 'POST':
+def server():
+	if request.method == 'POST' and 'file' in request.files:
 		# check if the post request has the file part
-		if 'file' not in request.files:
-			print('No file part')
-			return
 		file = request.files['file']
 		if file.filename == '':
-			print('No selected file')
-			return 
+			verboses('No selected file')
 		if file:
 			json_ = file.read()
 			json_ = json_to_json(json_,tokenizer=tokenizer,parser=parser)
 			return(json_to_response(json_))
 	
+	if request.headers['Content-Type'] == 'application/json':
+		if 'text' in request.get_json():
+			try:
+				json_ = text_to_json(request.get_json()['text'])
+				json_ = json_to_json(json_, tokenizer, parser)
+				return(json_to_response(json_))
+			except Exception as e:
+				error_log(e)
+				return("Error while processing request for '{}'. Check {} for more information.\n".format(request.get_json()['text'],ERROR_FILE),500)
+		return(NO_TEXT_ERROR_D,400)
+	
+	if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+		if 'text' in request.form:
+			try:
+				json_ = text_to_json(request.form['text'])
+				json_ = json_to_json(json_, tokenizer, parser)
+				return(json_to_response(json_))
+			except Exception as e:
+				error_log(e)
+				return("Error while processing request for '{}'. Check {} for more information.\n".format(request.form['text'],ERROR_FILE),500)
+		return(NO_TEXT_ERROR_D,400)
+		
+	return("Unsupported media type.",415)
+
+def text_to_json(text):
+	json_ = '''{"text":"''' + text + '''"}'''
+	return json_
+
 def json_to_response(json_):
 	response = make_response(json_)
 
@@ -68,16 +92,10 @@ def json_to_response(json_):
 	return(response)
 
 # PROCESSING STUFF
-	
 def json_to_json(json_,tokenizer,parser,):
 	"""Coordinates the entire pipeline"""
 
 	verbose("Starting pipeline")
-	
-	# TODO: alrighty, this is where we're starting
-	# I need some pre-annotated text
-	# so let's try some OGER JSON output
-	# Use them as tokens for a new spaCy object
 
 	try:
 		verbose("Convert JSON to spaCy...")
@@ -92,7 +110,11 @@ def json_to_json(json_,tokenizer,parser,):
 		raise(e)
 		
 def json_to_spacy(json_,tokenizer=False,parser=False):
-	json_ = json.loads(str(json_,encoding='utf-8'))
+	try:
+		json_ = str(json_,encoding="utf-8")
+	except Exception:
+		pass
+	json_ = json.loads(json_)
 	text = json_['text']
 
 	if not tokenizer:
@@ -101,59 +123,64 @@ def json_to_spacy(json_,tokenizer=False,parser=False):
 	if not parser:
 		parser = spacy.load('en',disable=['tagger'])
 
-	denotations = []
-	# start, end, length, id
-	for denotation in json_['denotations']:
-		id_ = denotation['id']
-		begin = denotation['span']['begin']
-		end = denotation['span']['end']
-		length = end - begin
-		obj = denotation['obj']
-		denotations.append({'id':id_,'begin':begin,'end':end,'length':length, 'obj':obj})
-
-	denotations_begins = {}
-	for denotation in denotations:
-		if denotation['begin'] not in denotations_begins:
-			denotations_begins[denotation['begin']] = []
-		denotations_begins[denotation['begin']].append(denotation)
-
-	longest_denotations = []
-	current = -1
-	for key, denotations in denotations_begins.items():
-		if denotations[0]['begin'] > current:
-			longest_denotation = max(denotations, key = lambda i: i['length'])
-			longest_denotations.append(longest_denotation)
-			current = longest_denotation['end']
-
-	tokens = []
-	tokens_ws = []
-	current_denotation = 0
-	advancement = 0
-	endgame = False
-	for token in tokenizer(text):
-		if token.idx < advancement:
-			continue
-
-		if token.idx + len(token.text_with_ws) <= longest_denotations[current_denotation]['begin'] or endgame:
-			tokens.append(token.text)
-			tokens_ws.append(len(token.text) != len(token.text_with_ws))
-
-		else:
-			begin = longest_denotations[current_denotation]['begin']
-			end = longest_denotations[current_denotation]['end']
-			token_text = text[begin:end]
-			tokens.append(token_text)
-			tokens_ws.append(text[end:end+1] == ' ')
-
-			advancement = end
-
-			if current_denotation + 1 < len(longest_denotations):
-				current_denotation += 1
+	if 'denotations' in json_:
+		denotations = []
+		# start, end, length, id
+		for denotation in json_['denotations']:
+			id_ = denotation['id']
+			begin = denotation['span']['begin']
+			end = denotation['span']['end']
+			length = end - begin
+			obj = denotation['obj']
+			denotations.append({'id':id_,'begin':begin,'end':end,'length':length, 'obj':obj})
+		
+		denotations_begins = {}
+		for denotation in denotations:
+			if denotation['begin'] not in denotations_begins:
+				denotations_begins[denotation['begin']] = []
+			denotations_begins[denotation['begin']].append(denotation)
+		
+		longest_denotations = []
+		current = -1
+		for key, denotations in denotations_begins.items():
+			if denotations[0]['begin'] > current:
+				longest_denotation = max(denotations, key = lambda i: i['length'])
+				longest_denotations.append(longest_denotation)
+				current = longest_denotation['end']
+		
+		tokens = []
+		tokens_ws = []
+		current_denotation = 0
+		advancement = 0
+		endgame = False
+		for token in tokenizer(text):
+			if token.idx < advancement:
+				continue
+		
+			if token.idx + len(token.text_with_ws) <= longest_denotations[current_denotation]['begin'] or endgame:
+				tokens.append(token.text)
+				tokens_ws.append(len(token.text) != len(token.text_with_ws))
+		
 			else:
-				endgame = True
-
-	doc = spacy.tokens.Doc(tokenizer.vocab, words=tokens,
-		  spaces=tokens_ws)
+				begin = longest_denotations[current_denotation]['begin']
+				end = longest_denotations[current_denotation]['end']
+				token_text = text[begin:end]
+				tokens.append(token_text)
+				tokens_ws.append(text[end:end+1] == ' ')
+		
+				advancement = end
+		
+				if current_denotation + 1 < len(longest_denotations):
+					current_denotation += 1
+				else:
+					endgame = True
+	
+		doc = spacy.tokens.Doc(tokenizer.vocab, words=tokens,
+			  spaces=tokens_ws)
+	
+	else:
+		tokens=[str(token.text) for token in tokenizer(text)]		
+		doc = spacy.tokens.Doc(tokenizer.vocab, words=tokens)
 
 	for name, proc in parser.pipeline:
 		doc = proc(doc)
@@ -197,10 +224,17 @@ def spacy_to_json(doc,text=False,annotations=False):
 		relation_dict["obj"] = "T{}".format(token.head.i)
 		relation_dict["pred"] = token.dep_
 		pre_json["relations"].append(relation_dict)
-
+	
 	if annotations:
-		annos = json.loads(str(annotations,encoding='utf-8')
+		try:
+			annotations = str(annotations,encoding="utf-8")
+		except Exception:
+			pass
+		
+		annos = json.loads(annotations)
+
 		if 'denotations' in annos:
+
 			deno_ids = set([deno['id'] for deno in pre_json['denotations']])
 			new_deno_ids = set([deno['id'] for deno in annos['denotations']])
 			if deno_ids.intersection(new_deno_ids): 
@@ -214,12 +248,11 @@ def spacy_to_json(doc,text=False,annotations=False):
 							relation['obj'] = relation['obj'] + '*'
 							relation['subj'] = relation['obj'] + '*'
 						except:
-							i=0
+							pass
 
 			pre_json["denotations"].extend(annos['denotations'])
 
-
-
+		
 		if 'relations' in annos:
 			rel_ids = set([rel['id'] for rel in pre_json['relations']])
 			new_rel_ids = set([rel['id'] for rel in annos['relations']])
@@ -227,6 +260,7 @@ def spacy_to_json(doc,text=False,annotations=False):
 				for relation in annos['relations']:
 					relation['id'] = relation['id'] + '*'
 			pre_json["relations"].extend(annos['relations'])
+		
 		if 'text' in annos and pre_json['text'] == False:
 			pre_json['text'] = annos['text']
 
--- readme.md
+++ readme.md
@@ -1,16 +1,15 @@
-### Installation
+### Installation / Running
 
-`python -m spacy download en`
+* It's in `/mnt/storage/karr/projects/clontogene/blah5`
+* Install `pip -r install requirements.txt`
+* python -m spacy download en`
 
 ### Use
 
-`curl -F file='@data/6234315.json' http://0.0.0.0:61455/ > out.json`
+* `curl -F file='@data/6234315.json' http://0.0.0.0:61455/ > out.json`
+* PubAnnotation style: `curl -d text="example text" URL_of_annotation_web_service`
 
 ### To Do
 
-* Make it interoperable with PubAnnotation
-
-
-
-
-
+* Test integration with PubAnnotation
+* Automatically get OGER annos
